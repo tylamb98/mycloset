194c318898775ed99ffea81871e9c09266a23586
tylamb98 <tlamb2@neiu.edu>
2021-10-03 18:07:56 -0500

    Initial commit

---
+++ src/main/java/edu/neiu/mycloset/MyclosetApplication.java
@@ -0,0 +1,13 @@
+package edu.neiu.mycloset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyclosetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyclosetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/neiu/mycloset/MyclosetApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neiu.mycloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyclosetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
