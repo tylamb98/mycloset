73671d33f87b3fc6303b811e9be22ac319476c3a
tylamb98 <tlamb2@neiu.edu>
2021-10-12 17:25:58 -0500

    added post mapping, added Getmapping, added display html, altered add outfit html

--- src/main/java/edu/neiu/mycloset/controllers/OutfitController.java
+++ src/main/java/edu/neiu/mycloset/controllers/OutfitController.java
@@ -4,16 +4,32 @@ import edu.neiu.mycloset.models.Outfit;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/outfit")
 public class OutfitController {
 
     @GetMapping
-    public String addOutfit (Model model) {
-        model.addAttribute("outfit", new Outfit());
-        return "add-outfit";
+    public String displayOutfit (Model model) {
+        model.addAttribute("outfit", new Outfit()); //Bind  with outfit html object.
+        return "add-outfit";        //returning the view
     }
+
+    @GetMapping("/display")
+    public String displayFullOutfit (@ModelAttribute("outfit") Object attrs, Model model) {
+        model.addAttribute("fullOufit", attrs);
+        return "display-outfit";
+    }
+
+    @PostMapping
+    public String handleOutfitForm(@ModelAttribute("outfit") Outfit outfit, RedirectAttributes attrs) {
+        attrs.addFlashAttribute("outfit",outfit.getTop() + " and " + outfit.getBottom() + " with " + outfit.getShoes());
+        return "redirect:/outfit/display";
+    }
+
 
 }
